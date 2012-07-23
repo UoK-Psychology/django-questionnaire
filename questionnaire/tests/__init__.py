680d8e9359e3ae9a33b819d33d7e27ebd208f8d9
Zaim <mzd2@kent.ac.uk>
2012-07-23 12:11:43 +0100

    testing on SetUp : needs opinion

---
+++ questionnaire/tests/__init__.py
@@ -0,0 +1,1 @@
+from questionnaire.tests.views import *
--- questionnaire/tests/views.py
+++ questionnaire/tests/views.py
@@ -1,5 +1,11 @@
-
+from questionnaire.views import *
+from questionnaire.models import Question,Questionnaire,QuestionGroup,Question_order, QuestionGroup_order
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import Client
+from django.core.urlresolvers import reverse
+
+
 
 class QuestionnaireViewTests(TestCase):
     
@@ -23,12 +29,111 @@ class QuestionnaireViewTests(TestCase):
             1. A User we can login with
             2. A Questionnaire defined, which has 2 QuestionGroup defined, each with 1 question of each question type
         """
-        pass
+        self.user_test = User.objects.create_user('user_test', 'email@email.com', 'password')
+        self.client = Client()
+        
+        
+        
+        """
+        create the first six question of different type
+        """
+        self.question_test_charfield = Question.objects.create(
+                                                     label = 'question 1 -  charfield',
+                                                     field_type = 'charfield',
+                                                     selectoptions= None
+                                                     )
+        
+        self.question_test_textfield = Question.objects.create(
+                                                     label = 'question 2 -  textfield',
+                                                     field_type = 'textfield',
+                                                     selectoptions= None
+                                                     )
+        
+        self.question_test_booleanfield = Question.objects.create(
+                                                     label = 'question 3 -  booleanfield',
+                                                     field_type = 'booleanfield',
+                                                     selectoptions= None
+                                                     )
+
+        self.question_test_radioselectfield = Question.objects.create(
+                                                     label = 'question 4 -  radioselectfield',
+                                                     field_type = 'radioselectfield',
+                                                     selectoptions= 'Radio 1, Radio 2, Radio 3'
+                                                     ) 
+                                                   
+        self.question_test_select_dropdown_field = Question.objects.create(
+                                                     label = 'question 5 -  select_dropdown_field',
+                                                     field_type = 'select_dropdown_field',
+                                                     selectoptions= 'Drop 1, Drop 2, Drop 3'
+                                                     )
+                
+        self.question_test_multiplechoicefield = Question.objects.create(
+                                                     label = 'question 6 -  multiplechoicefield',
+                                                     field_type = 'multiplechoicefield',
+                                                     selectoptions= 'Multiple Choice 1, Multiple Choice 2, Multiple Choice 3'
+                                                     )       
+        
+
+        """
+        create a questiongroup and its set its many to many relation through question_order
+        """
+        self.questiongroup_test_group_1 = QuestionGroup.objects.create(
+                                                     name = 'question group test 1',
+                                                     questions = (self.question_test_charfield)                                  
+                                                                       )
+        self.questionorder_test_1 = Question_order.objects.create(
+                                                                  questiongroup = self.questiongroup_test_group_1,
+                                                                  question = self.question_test_charfield,
+                                                                  order_info = 1,
+                                                                  )
+        
+        
+        
+        
+        
+        self.questiongroup_test_group_1 = QuestionGroup.objects.create(
+                                                     name = 'question group test 1',
+                                                     questions = (self.question_test_textfield)                                  
+                                                                       )
+        self.questiongroup_test_group_1 = QuestionGroup.objects.create(
+                                                     name = 'question group test 1',
+                                                     questions = (self.question_test_booleanfield)                                  
+                                                                       )
+        self.questiongroup_test_group_2 = QuestionGroup.objects.create(
+                                                     name = 'question group test 2',
+                                                     questions = (self.question_test_radioselectfield)                                  
+                                                                       )
+        self.questiongroup_test_group_2 = QuestionGroup.objects.create(
+                                                     name = 'question group test 2',
+                                                     questions = (self.question_test_dropdown_field)                                  
+                                                                       )
+        self.questiongroup_test_group_2 = QuestionGroup.objects.create(
+                                                     name = 'question group test 2',
+                                                     questions = (self.question_test_multiplechoicefield)                                  
+                                                                       )
+        
+        """
+        Create one Questionnaire object and set its many to many relation with questiongroup through questiongroup_order
+        """        
+        self.questionnaire_test = Questionnaire.objects.create(
+                                                               name = 'Questionnaire_Test',
+                                                               questiongroup = (self.questiongroup_test_group_1, 1)
+                                                               )
+        self.questionnaire_test = Questionnaire.objects.create(
+                                                               name = 'Questionnaire_Test',
+                                                               questiongroup = (self.questiongroup_test_group_2, 2)
+                                                               )        
+
+        
+    
     def test_handle_next_questiongroup_form_no_user(self):
         
         """
             A get request to this view without a logged in user should redirect to the default login url
         """
+        response = self.client.get('/questionnaire/qs/1')
+        self.assertFalse(response.status_code, 200)    
+            
         self.assert_(False, 'Not yet implemented')
         
     def test_handle_next_questiongroup_form_get_valid_questionnaire_firsttime(self):
@@ -40,6 +145,7 @@ class QuestionnaireViewTests(TestCase):
             3. have a form in the context containing fields representing the first group in the questionnaire
             but unbound to any data (ie. not have any value associated with them)
         """
+        
          
         self.assert_(False, 'Not yet implemented')
         
