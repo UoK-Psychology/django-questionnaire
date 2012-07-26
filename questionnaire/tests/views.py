7ac77cc831cacad3ff6b13c121cab007137c0533
Zaim <mzd2@kent.ac.uk>
2012-07-26 11:17:58 +0100

    finished will make a pull request

--- questionnaire/tests/views.py
+++ questionnaire/tests/views.py
@@ -35,6 +35,8 @@ class QuestionnaireViewTests(TestCase):
         self.client = Client()
         self.user_test = User.objects.create_user('user', 'email@email.com', 'password')          
         self.answerset = AnswerSet.objects.create(user=self.user_test,questionnaire=testquestionnaire)
+
+        
         super(QuestionnaireViewTests,self).setUp()
             
     def test_handle_next_questiongroup_form_no_user(self):
@@ -87,9 +89,9 @@ class QuestionnaireViewTests(TestCase):
         """
         
         self.client.login(username='user', password='password') 
-        url = reverse('handle_first_questiongroup_form', kwargs={'questionnaire_id':0})
+        url = reverse('handle_first_questiongroup_form', kwargs={'questionnaire_id': 2})
         resp = self.client.get(url)
-        self.assertEqual(resp.status_code, 404, 'There are no questionnaire with id 0!')
+        self.assertEqual(resp.status_code, 404, 'There are no questionnaire with id 2!')
         
         
     def test_handle_next_questiongroup_form_post_success_firsttime(self):
@@ -137,29 +139,37 @@ class QuestionnaireViewTests(TestCase):
             3. It should redirect to the handle_next_questiongroup_form url specifying the questionnaire id and the 
             id of the next question group for the questionniare (in this case there is one as that is how we setup the fixture)
         """
-        self.client.login(username='user', password='password')
+        
         '''
         create object first, to simulate it is already answered! and then try to post the question id but different answer!
         '''
         self.question_answer_1 = QuestionAnswer.objects.create(question=Question.objects.get(pk=1),answer="charfield",answer_set=AnswerSet.objects.get(pk=1))
         self.question_answer_2 = QuestionAnswer.objects.create(question=Question.objects.get(pk=2),answer="textfield",answer_set=AnswerSet.objects.get(pk=1))
         self.question_answer_3 = QuestionAnswer.objects.create(question=Question.objects.get(pk=3),answer="True",answer_set=AnswerSet.objects.get(pk=1))  
+        
+        self.client.login(username='user', password='password')
         url = reverse('handle_first_questiongroup_form', kwargs={'questionnaire_id': 1})
-        post_data =  {u'1': [u'b'], u'2': [u'b'], u'3': [u'False']}
+        post_data =  {u'1': [u'b'], u'2': [u'b'], u'3': [u'True']}
         resp = self.client.post(url,post_data)
 
 
         self.assertEqual(302, resp.status_code)      
         self.assertEqual(resp['Location'], 'http://testserver/questionnaire/qs/1/2/')
         
+        '''
+        Check if the new post creates a new object and compare it is not the same answer as the previous one and check the count of the questionanswer
+        increased by 2 meaning a new object is made
+        '''
         self.assertEqual(QuestionAnswer.objects.get(pk=1).answer, 'charfield')
-        self.assertEqual(QuestionAnswer.objects.get(pk=4).answer, 'b')
+        self.assertEqual(QuestionAnswer.objects.get(pk=4).answer, 'b')                
+        self.assertNotEqual(QuestionAnswer.objects.get(pk=4).answer, QuestionAnswer.objects.get(pk=1).answer) 
+        self.assertEqual(QuestionAnswer.objects.all().count(), 5)
         
-        
-        self.assertNotEqual(QuestionAnswer.objects.get(pk=4).answer, QuestionAnswer.objects.get(pk=1).answer)
-        
-        
+        '''
+        check if there are only 1 AnswerSet object created which means there is no extra AnswerSet object created
+        '''               
         self.assertEqual(AnswerSet.objects.get(pk=1).user, User.objects.get(pk=1))
+        self.assertEqual(AnswerSet.objects.all().count(), 1 )
         
     def test_handle_next_questiongroup_form_post_failure(self):
         
@@ -173,6 +183,7 @@ class QuestionnaireViewTests(TestCase):
         
         self.client.login(username='user', password='password')
         post_data =  {u'1': [u'c'], u'2': [u'b'], u'3': [u'xxx']}
+        
         resp = self.client.post('/questionnaire/qs/1/',post_data)
         self.assertNotEqual(200, resp.status_code)
         
@@ -183,8 +194,8 @@ class QuestionnaireViewTests(TestCase):
             1. return a 200 HTTPResponse
             2. Render the finish.html template
         """
-        
-        resp = self.client.get('/questionnaire/finish/')
+        url = reverse('questionnaire_finish')
+        resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200, 'finish page should be shown')
         self.assertTemplateUsed('finish.html') 
         
@@ -193,9 +204,15 @@ class QuestionnaireViewTests(TestCase):
             GET request to ''display_question_answer'' with an invalid question id (ie it doesn't exist)(but logged in) should:
             1. yield a http 404 response
         """
+        '''
+        This test will fail because the view for display_questionnaire have not been fully implemented to use questionnaire_id to call each of the
+        question group.
+        i.e. still using objects.get.all() - this however will pass if the actual view code have been implemented
+        '''
         self.client.login(username='user', password='password') 
-        resp = self.client.get('/questionnaire/Answer/0/')
-        self.assertEqual(resp.status_code, 404, 'There are no questionnaire with id 0!')
+        url = reverse('display_question_answer', kwargs={'questionnaire_id':2})
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 404, 'There are no questionnaire with id 2!')
         
         
     def test_display_question_answer_valid_questionnaire_not_logged_in(self):
@@ -203,7 +220,8 @@ class QuestionnaireViewTests(TestCase):
             GET request to ''display_question_answer'' without being logged in should:
             1. redirect to the default login url
         """
-        response = self.client.get('/questionnaire/Answer/1')
+        url = reverse('display_question_answer', kwargs={'questionnaire_id':1})
+        response = self.client.get(url)
         self.assertEquals (302, response.status_code)
         
         
@@ -215,8 +233,9 @@ class QuestionnaireViewTests(TestCase):
             2. template used should be questionAnswer.html
             3. context should not contain any answers (which will cause the template to show that you have no answers)
         """
-        self.client.login(username='user', password='password')    
-        resp = self.client.get('/questionnaire/Answer/1')
+        self.client.login(username='user', password='password')
+        url = reverse('display_question_answer', kwargs={'questionnaire_id':1})    
+        resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200, 'user authenticated and can access the page')
         self.assertTemplateUsed('questionanswer.html')         
         self.assertEqual(len(resp.context['context']), 0)
@@ -237,8 +256,9 @@ class QuestionnaireViewTests(TestCase):
         self.question_answer_2 = QuestionAnswer.objects.create(question=Question.objects.get(pk=2),answer="textfield",answer_set=AnswerSet.objects.get(pk=1))
         self.question_answer_3 = QuestionAnswer.objects.create(question=Question.objects.get(pk=3),answer="True",answer_set=AnswerSet.objects.get(pk=1))
 
-        self.client.login(username='user', password='password')    
-        resp = self.client.get('/questionnaire/Answer/1')
+        self.client.login(username='user', password='password')
+        url = reverse('display_question_answer', kwargs={'questionnaire_id':1})    
+        resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200, 'user authenticated and can access the page')
         self.assertTemplateUsed('questionanswer.html') 
         self.assertLess(len(resp.context['context']), 6)
@@ -261,8 +281,9 @@ class QuestionnaireViewTests(TestCase):
         self.question_answer_6 = QuestionAnswer.objects.create(question=Question.objects.get(pk=6),answer="[u'Multiple Choice 1']",answer_set=AnswerSet.objects.get(pk=1))
         
         
-        self.client.login(username='user', password='password')            
-        resp = self.client.get('/questionnaire/Answer/1')        
+        self.client.login(username='user', password='password')
+        url = reverse('display_question_answer', kwargs={'questionnaire_id':1})            
+        resp = self.client.get(url)        
         self.assertEqual(resp.status_code, 200, 'user authenticated and can access the page')
         self.assertTemplateUsed('questionanswer.html')    
          
@@ -289,8 +310,9 @@ class QuestionnaireViewTests(TestCase):
         
         
         
-        self.client.login(username='user', password='password')    
-        resp = self.client.get('/questionnaire/Answer/1')
+        self.client.login(username='user', password='password')
+        url = reverse('display_question_answer', kwargs={'questionnaire_id':1})    
+        resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200, 'user authenticated and can access the page')
         self.assertTemplateUsed('questionanswer.html') 
         self.assertGreater(len(resp.context['context']), 6)
