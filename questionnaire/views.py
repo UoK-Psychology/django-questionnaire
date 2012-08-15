089a0baaa6d6be14e5ebf22bb02062d017ddc79d
Ayoola Adegbite <ayoola_al@yahoo.com>
2012-08-15 23:54:31 +0100

    reviewcode view

--- questionnaire/views.py
+++ questionnaire/views.py
@@ -105,29 +105,39 @@ def display_question_answer(request,questionnaire_id,questiongroup_id):
     request user is user that has answered the given questionnnaire questiongroup
     '''
     questiongroup_id=int(questiongroup_id)
-    
 
-    if request.method=='GET': #TODO: is it really nescessary to check that it is a GET request, worst comes to worse if someone posts to this you will just treat it like a GET? Additionaly if you don't return anything from this function then an error will be raised so if it is important that it be a get request there should be an else that either raises an appropriate message (404?!), or renders a hepful erorr message page
-        user=request.user
-        this_questionnaire=get_object_or_404(Questionnaire,pk=questionnaire_id)
-        this_questiongroup=get_object_or_404(QuestionGroup,pk=questiongroup_id)
+    # TODO: is it really nescessary to check that it is a GET request, worst comes to worse if someone posts to this you will just treat it like a GET? Additionaly if you don't return anything from this function then an error will be raised so if it is important that it be a get request there should be an else that either raises an appropriate message (404?!), or renders a hepful erorr message page
+    user=request.user
+    this_questionnaire=get_object_or_404(Questionnaire,pk=questionnaire_id)
+    this_questiongroup=get_object_or_404(QuestionGroup,pk=questiongroup_id)
         
         #TODO can you explain this?
-        q_list=QuestionAnswer.objects.values('question','answer_set').annotate(Max('id'))
-        answer_max_id_list=q_list.values_list('id__max',flat=True)
+## EXPLAINATION  given that max id of an object  represent  most recent created object otherwise you have to get created date timestamp .Max is django built in fuction. this retrieve max ids for Questionanswer object  based on question and answer_set ,annotate constraint the objects to the only objects with maximum id
+## second line flatten the queryset to list of only max-ids of the most recent  questionanswer .
+    q_list=QuestionAnswer.objects.values('question','answer_set').annotate(Max('id'))  
+    answer_max_id_list=q_list.values_list('id__max',flat=True)
        
         
-        #TODO: Why do you need to get the ordered list of groups? you have specified a specific question group_id as an argument for the function?
-        orderedgroups = QuestionGroup_order.objects.filter(questionnaire= this_questionnaire).order_by('order_info')#TODO: use .get_ordered_groups() for this as it abstracts the need to know about the ordering implementation and you won't need the next line as it returns a list of questiongroups
-        groups_list=[(x.questiongroup) for x in orderedgroups]
+    #TODO: Why do you need to get the ordered list of groups? you have specified a specific question group_id as an argument for the function?
+#   orderedgroups = QuestionGroup_order.objects.filter(questionnaire= this_questionnaire).order_by('order_info')#TODO: use .get_ordered_groups() for this as it abstracts the need to know about the ordering implementation and you won't need the next line as it returns a list of questiongroups
+    orderedgroups=this_questionnaire.get_ordered_groups()
+    
+##EXPLAINATION  group list is passed to the template to display links to  other question groups in the  given questionnaire so it make easier for user to navigate to other questionanswer display in the questionnaire
+    groups_list=[(x.questiongroup) for x in orderedgroups]
         
-        #TODO: this seems a quite complex, and is difficult to follow, either add a comment to explain what you are doing, or consider refactoring to make it simpler?
-        y=QuestionAnswer.objects.filter(Q(answer_set__user_id=user,answer_set__questiongroup=this_questiongroup,answer_set__questionnaire=this_questionnaire)).filter(id__in=answer_max_id_list)          
-        questionanswer=[(x.question.id,x.question.label ,x.answer) for x in y]
+    #TODO: this seems a quite complex, and is difficult to follow, either add a comment to explain what you are doing, or consider refactoring to make it simpler?
+   ##EXPLAINATION  using django Complex lookups with Q objects use for join query , it retrieve all the questionanswer related objects where the user is this user for this question group and this questionnaire
+   ##  it does this by span relationship i.e double underscore  between QuestionAnswer  answerset attribute  and Answerset object attributes i.e user ,questiongroup,questionnaire this referential relationship is already defined in the models
+   ##   since we are only interested in the most recent questionanswer filter(id__in=answer_max_id_list) further filter all related questionanswer objects to the one qa objects in the max id list .
+   ##   [(x.question.id,x.question.label ,x.answer)for x in y] .  this retrieve the question label and the answer for  each questionanswer in the most recent questionanswer put them in a list.
+    y=QuestionAnswer.objects.filter(Q(answer_set__user_id=user,answer_set__questiongroup=this_questiongroup,answer_set__questionnaire=this_questionnaire)).filter(id__in=answer_max_id_list)          
+    print y
+    questionanswer=[(x.question.id,x.question.label ,x.answer) for x in y]
 
-        context=questionanswer#TODO: Be careful using the variable context, it has special significance, and using this way would be confusing to the reader of your code
+    #TODO: Be careful using the variable context, it has special significance, and using this way would be confusing to the reader of your code
         #TODO: you don't need to explicitly put user into your responses context, it will get put there by virtue of using context_instance=RequestContext(request)
-        return render_to_response('display_questionanswer.html',{'context':context,'user':user,'questionnaire':this_questionnaire,'questiongroup_id':questiongroup_id,'groups_list':groups_list,},context_instance=RequestContext(request))
+    ## EXPLAINATION : context variable REMOVED
+    return render_to_response('display_questionanswer.html',{'questionanswer':questionanswer,'questionnaire':this_questionnaire,'questiongroup_id':questiongroup_id,'groups_list':groups_list,},context_instance=RequestContext(request))
 
 
 
@@ -142,13 +152,14 @@ def edit_question_answer(request,questionnaire_id,questiongroup_id):
     '''
     
     user=request.user   
-    questionnaire_id=int(questionnaire_id)#TODO: Do you really need to cast to an int?
+#    questionnaire_id=int(questionnaire_id)#TODO: Do you really need to cast to an int?
     questiongroup_id=int(questiongroup_id)#TODO: Do you really need to cast to an int?
     this_questionnaire=get_object_or_404(Questionnaire,pk=questionnaire_id)
     this_questiongroup=get_object_or_404(QuestionGroup,pk=questiongroup_id)
         
     #TODO: use .get_ordered_groups() for this as it abstracts the need to know about the ordering implementation and you won't need the next line as it returns a list of questiongroups
-    orderedgroups = QuestionGroup_order.objects.filter(questionnaire= this_questionnaire).order_by('order_info')    
+#    orderedgroups = QuestionGroup_order.objects.filter(questionnaire= this_questionnaire).order_by('order_info')
+    orderedgroups=this_questionnaire.get_ordered_groups() 
     groups_list=[(x.questiongroup.id) for x in orderedgroups]
 
     editForm= create_question_answer_edit_form(user,this_questionnaire,this_questiongroup) 
@@ -181,11 +192,11 @@ def edit_question_answer(request,questionnaire_id,questiongroup_id):
 #TODO: you should be able to refactor this so that you dont duplicate all of this code, if you get rid of the else clauses, if you get this far then you are either
 #dealing with an invalid form or a get request, and you should be able to hand them in the same way
             return render_to_response('edit_questionanswer_form.html', 
-       {'form': form,'user':user,'questionnaire':this_questionnaire,'questiongroup_id':questiongroup_id,'groups_list':groups_list,},context_instance=RequestContext(request))
+       {'form': form,'questionnaire':this_questionnaire,'questiongroup_id':questiongroup_id,'groups_list':groups_list,},context_instance=RequestContext(request))
     #TODO: you don't need to explicitly put user into your responses context, it will get put there by virtue of using context_instance=RequestContext(request)
     else :
         return render_to_response('edit_questionanswer_form.html', 
-                                      {'form': editForm,'user':user,'questionnaire':this_questionnaire,'questiongroup_id':questiongroup_id,'groups_list':groups_list,},context_instance=RequestContext(request))
+                                      {'form': editForm,'questionnaire':this_questionnaire,'questiongroup_id':questiongroup_id,'groups_list':groups_list,},context_instance=RequestContext(request))
 
 #End refactor target
 
@@ -208,7 +219,8 @@ def all_question_answers_for_questiongroup(request,user_id,questionnaire_id,ques
         this_questiongroup=get_object_or_404(QuestionGroup,pk=questiongroup_id)
         
         #TODO: use .get_ordered_groups() for this as it abstracts the need to know about the ordering implementation and you won't need the next line as it returns a list of questiongroups
-        orderedgroups = QuestionGroup_order.objects.filter(questionnaire= this_questionnaire).order_by('order_info')    
+#        orderedgroups = QuestionGroup_order.objects.filter(questionnaire= this_questionnaire).order_by('order_info') 
+        orderedgroups=this_questionnaire.get_ordered_groups()   
         groups_list=[(x.questiongroup) for x in orderedgroups]
         
         #TODO explain what this does
@@ -218,10 +230,10 @@ def all_question_answers_for_questiongroup(request,user_id,questionnaire_id,ques
        
         qs= sorted(set(questionanswer),key=itemgetter(0))            
         questionanswer_list = list(map(itemgetter(0), groupby(qs)))        
-        context= questionanswer_list
+#        context= questionanswer_list
 
         #
-        return render_to_response('all_questionanswers.html',{'context':context,'user':user,'questionnaire':this_questionnaire,'questiongroup_id':questiongroup_id,'groups_list':groups_list,},context_instance=RequestContext(request))
+        return render_to_response('all_questionanswers.html',{'questionanswer_list':questionanswer_list,'user':user,'questionnaire':this_questionnaire,'questiongroup_id':questiongroup_id,'groups_list':groups_list,},context_instance=RequestContext(request))
 
 
 
@@ -231,18 +243,19 @@ def questionnaire_detail_list(request,questionnaire_id):
     show  detail list as links  for all questiongroups in a given questionnaire ordered by order_info
     links can be use to edit or display given  questionanswer for questiongroups in questionnaire
     '''
-    if request.method=='GET':#TODO: is this really nescessay?
+   
         
-        this_questionnaire=get_object_or_404(Questionnaire,pk=questionnaire_id)
+    this_questionnaire=get_object_or_404(Questionnaire,pk=questionnaire_id)
         
-        #TODO: use .get_ordered_groups() for this as it abstracts the need to know about the ordering implementation and you won't need the next line as it returns a list of questiongroups
-        orderedgroups = QuestionGroup_order.objects.filter(questionnaire= this_questionnaire).order_by('order_info')    
-        groups_list=[(x.questiongroup) for x in orderedgroups]
+    #TODO: use .get_ordered_groups() for this as it abstracts the need to know about the ordering implementation and you won't need the next line as it returns a list of questiongroups
+#        orderedgroups = QuestionGroup_order.objects.filter(questionnaire= this_questionnaire).order_by('order_info') 
+    orderedgroups=this_questionnaire.get_ordered_groups()    
+    groups_list=[(x.questiongroup) for x in orderedgroups]
         
         #TODO: careful about using context as a variable now it has special significance in views
-        context = groups_list
+   
         #TODO: you don't need to explicitly put user into your responses context, it will get put there by virtue of using context_instance=RequestContext(request)
-        return render_to_response('questionnaire_detail.html',{'context':context, 'questionnaire':this_questionnaire,},context_instance=RequestContext(request))
+    return render_to_response('questionnaire_detail.html',{'groups_list': groups_list, 'questionnaire':this_questionnaire,},context_instance=RequestContext(request))
 
     
     
