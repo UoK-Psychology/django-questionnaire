3ee806c81eb2a859ec68be75dfdc33a550000904
Jason Marshall <ctrlspc@gmail.com>
2012-09-17 20:06:03 +0100

    implemented the last test and fixed a bug with the
    questionnaire_id

--- questionnaire/tests/views.py
+++ questionnaire/tests/views.py
@@ -291,7 +291,7 @@ class DoQuestionnaireTests(TestCase):
             
         self.client.login(username='user', password='password')
         
-        url = reverse('handle_next_questiongroup_form', kwargs={'questionnaire_id': 1, 'order_index':0, 'group_limit':1})
+        url = reverse('handle_next_questiongroup_form', kwargs={'questionnaire_id': test_questionnaire.id, 'order_index':0, 'group_limit':1})
         post_data =  {u'1': [u'b'], u'2': [u'b'], u'3': 1} #a valid post for this questiongroup
         resp = self.client.post(url,post_data)
 
@@ -318,15 +318,15 @@ class DoQuestionnaireTests(TestCase):
             
         self.client.login(username='user', password='password')
         
-        url = reverse('handle_next_questiongroup_form', kwargs={'questionnaire_id': 1, 'order_index':0, 'group_limit':2})
+        url = reverse('handle_next_questiongroup_form', kwargs={'questionnaire_id': test_questionnaire.id, 'order_index':0, 'group_limit':2})
         post_data =  {u'1': [u'b'], u'2': [u'b'], u'3': 1} #a valid post for this questiongroup
         resp = self.client.post(url,post_data)
 
         self.assertEqual(resp.status_code, 302)   
-        redirect_url = reverse('handle_next_questiongroup_form', kwargs={'questionnaire_id': 1, 'order_index':1,#order incremented by 1
+        redirect_url = reverse('handle_next_questiongroup_form', kwargs={'questionnaire_id': test_questionnaire.id, 'order_index':1,#order incremented by 1
                                                                                      'group_limit':1}) #limit reduced by 1 
         
-        self.assertEqual(resp['Location'], redirect_url)#this is what is configured as the finish url in the url conf
+        self.assertEqual(resp['Location'], 'http://testserver' + redirect_url)#this is what is configured as the finish url in the url conf
 
         
     def test_valid_post_with_limit_of_greater_than_one_groups_not_avaialble(self):
@@ -339,7 +339,21 @@ class DoQuestionnaireTests(TestCase):
             will simply terminate as usual after the last group.
         '''
         
-        self.assertTrue(False)
+        test_questionnaire = Questionnaire.objects.create(name='test_questionnaire')
+        test_group = QuestionGroup.objects.get(pk=1) #we know that this is in the db from the fixture
+        
+        #only add the group once this time, so there is only one group in the list
+        test_questionnaire.add_question_group(test_group)
+            
+        self.client.login(username='user', password='password')
+        
+        url = reverse('handle_next_questiongroup_form', kwargs={'questionnaire_id': test_questionnaire.id, 'order_index':0, 'group_limit':2})
+        post_data =  {u'1': [u'b'], u'2': [u'b'], u'3': 1} #a valid post for this questiongroup
+        resp = self.client.post(url,post_data)
+
+        self.assertEqual(resp.status_code, 302)     
+        self.assertEqual(resp['Location'], 'http://testserver/questionnaire/finish/')#this is what is configured as the finish url in the url conf
+
         
         
         
